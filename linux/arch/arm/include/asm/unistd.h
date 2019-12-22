e2a58cf1ec0bbd3b6257d7f7fe1744acd3c1a598
roman <piececakeof@gmail.com>
2019-12-22 14:59:27 +0200

    add linux headers

--- linux/arch/arm/include/asm/unistd.h
+++ linux/arch/arm/include/asm/unistd.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 /*
  *  arch/arm/include/asm/unistd.h
  *
@@ -13,57 +14,28 @@
 #ifndef __ASM_ARM_UNISTD_H
 #define __ASM_ARM_UNISTD_H
 
-#include <uapi/asm/unistd.h>
-#include <asm/unistd-nr.h>
+#define __NR_OABI_SYSCALL_BASE	0x900000
 
-#define __ARCH_WANT_NEW_STAT
-#define __ARCH_WANT_STAT64
-#define __ARCH_WANT_SYS_GETHOSTNAME
-#define __ARCH_WANT_SYS_PAUSE
-#define __ARCH_WANT_SYS_GETPGRP
-#define __ARCH_WANT_SYS_NICE
-#define __ARCH_WANT_SYS_SIGPENDING
-#define __ARCH_WANT_SYS_SIGPROCMASK
-#define __ARCH_WANT_SYS_OLD_MMAP
-#define __ARCH_WANT_SYS_OLD_SELECT
-#define __ARCH_WANT_SYS_UTIME
-
-#if !defined(CONFIG_AEABI) || defined(CONFIG_OABI_COMPAT)
-#define __ARCH_WANT_SYS_TIME
-#define __ARCH_WANT_SYS_IPC
-#define __ARCH_WANT_SYS_OLDUMOUNT
-#define __ARCH_WANT_SYS_ALARM
-#define __ARCH_WANT_SYS_OLD_GETRLIMIT
-#define __ARCH_WANT_OLD_READDIR
-#define __ARCH_WANT_SYS_SOCKETCALL
+#if defined(__thumb__) || defined(__ARM_EABI__)
+#define __NR_SYSCALL_BASE	0
+#include <asm/unistd-eabi.h>
+#else
+#define __NR_SYSCALL_BASE	__NR_OABI_SYSCALL_BASE
+#include <asm/unistd-oabi.h>
 #endif
-#define __ARCH_WANT_SYS_FORK
-#define __ARCH_WANT_SYS_VFORK
-#define __ARCH_WANT_SYS_CLONE
+
+#include <asm/unistd-common.h>
+#define __NR_sync_file_range2		__NR_arm_sync_file_range
 
 /*
- * Unimplemented (or alternatively implemented) syscalls
+ * The following SWIs are ARM private.
  */
-#define __IGNORE_fadvise64_64
-#define __IGNORE_migrate_pages
-
-#ifdef __ARM_EABI__
-/*
- * The following syscalls are obsolete and no longer available for EABI:
- *  __NR_time
- *  __NR_umount
- *  __NR_stime
- *  __NR_alarm
- *  __NR_utime
- *  __NR_getrlimit
- *  __NR_select
- *  __NR_readdir
- *  __NR_mmap
- *  __NR_socketcall
- *  __NR_syscall
- *  __NR_ipc
- */
-#define __IGNORE_getrlimit
-#endif
+#define __ARM_NR_BASE			(__NR_SYSCALL_BASE+0x0f0000)
+#define __ARM_NR_breakpoint		(__ARM_NR_BASE+1)
+#define __ARM_NR_cacheflush		(__ARM_NR_BASE+2)
+#define __ARM_NR_usr26			(__ARM_NR_BASE+3)
+#define __ARM_NR_usr32			(__ARM_NR_BASE+4)
+#define __ARM_NR_set_tls		(__ARM_NR_BASE+5)
+#define __ARM_NR_get_tls		(__ARM_NR_BASE+6)
 
 #endif /* __ASM_ARM_UNISTD_H */
